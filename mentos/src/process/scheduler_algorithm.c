13509b745a9d6fa66b9f017b489fa6d692588aad
mattia <mattia.corradi.tr@gmail.com>
2019-05-13 15:51:00 +0200

    adding other scheduling algorithm

--- mentos/src/process/scheduler_algorithm.c
+++ mentos/src/process/scheduler_algorithm.c
@@ -26,26 +26,25 @@ task_struct *pick_next_task(runqueue_t *runqueue, time_t delta_exec)
 	
 	next = list_entry(nNode, struct task_struct, run_list);
 
-
-
-
 	//=======================================================================
 #elif defined(SCHEDULER_PRIORITY)
+
 	//==== Implementatin of the Priority Scheduling algorithm ===============
 
 	// get the first element of the list
-	next = list_entry(/*...*/);
+	next = list_entry(runqueue->queue.next, struct task_struct, run_list);
 
 	// Get its static priority.
-	time_t min = /*...*/
+	time_t min = next->se.prio;
 
 	list_head *it;
 	// Inter over the runqueue to find the task with the smallest priority value
 	list_for_each (it, &runqueue->queue) {
-		task_struct *entry = list_entry(/*...*/);
+		task_struct *entry = list_entry(it, struct task_struct, run_list);
 		// Check entry has a lower priority
-		if (/*...*/) {
-			/*...*/
+		if (min > entry-> se.prio) {
+			next = entry;
+			min = next->se.prio;
 		}
 	}
 
@@ -55,21 +54,34 @@ task_struct *pick_next_task(runqueue_t *runqueue, time_t delta_exec)
 
 	// Get the weight of the current process.
 	// (use GET_WEIGHT macro!)
-	int weight = /*...*/
-
+	int weight = GET_WEIGHT(runqueue->curr->se.prio);
+	
 	if (weight != NICE_0_LOAD) {
 		// get the multiplicative factor for its delta_exec.
-		double factor = /*...*/
+		double factor = NICE_0_LOAD / weight;
 
 		// weight the delta_exec with the multiplicative factor.
-		delta_exec = // ...
+		delta_exec *= factor;
 	}
 
-	// Update vruntime of the current process.
-	// ...
+	// Update vruntime of the current process
+	runqueue->curr->se.vruntime += delta_exec;
 
+	next = list_entry(runqueue->queue.next, struct task_struct, run_list);
+	time_t min = next->se.vruntime;
+	
+	list_head *it;
 	// Inter over the runqueue to find the task with the smallest vruntime value
-	// ...
+	list_for_each (it, &runqueue->queue) {
+		task_struct *entry = list_entry(it, struct task_struct, run_list);
+		// Check entry has a lower priority
+		if (min > entry-> se.vruntime) {
+			next = entry;
+			min = next->se.vruntime;
+		}
+	}
+
+
 
 	//========================================================================
 #else
